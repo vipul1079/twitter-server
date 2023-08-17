1fd9b6413ef7f9a34780e15fe4d2ff0a02d49b91
vipul1079 <vipulsharma1079@gmail.com>
2023-08-17 20:57:29 +0530

    Everything works fine just a little issue with cors idk what

---
+++ src/app/index.ts
@@ -0,0 +1,36 @@
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { User } from "./user";
+
+export async function initServer() {
+  const app = express();
+
+  const options = {
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+  };
+  app.use(cors(options));
+  app.use(bodyParser.json());
+
+  const graphqlServer = new ApolloServer({
+    typeDefs: `
+    ${User.types}
+        type Query {
+            ${User.queries}
+        }
+        `,
+    resolvers: {
+      Query: {
+        ...User.resolvers.queries,
+      },
+    },
+  });
+  await graphqlServer.start();
+
+  app.use("/graphql", expressMiddleware(graphqlServer));
+
+  return app;
+}
